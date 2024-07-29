338d7851c6618490f87f92902e25c9e947cf4ce1
Ilef01 <bchinyyilef@gmail.com>
2024-07-29 18:31:38 +0200

    initial commit

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+""" Pascal's triangle solution """
+
+
+def pascal_triangle(n):
+    if n <= 0:
+        return []
+
+    # Base cases
+    if n == 1:
+        return [[1]]
+    if n == 2:
+        return [[1], [1, 1]]
+    
+    list_of_lists=pascal_triangle(n-1)
+    last_list=list_of_lists[-1]
+    current_list=[1]
+
+    for i in range(len(last_list)-1):
+        current_list.append(last_list[i] + last_list[i+1])
+    current_list.append(1)
+    list_of_lists.append(current_list)
+    return list_of_lists
